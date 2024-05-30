07079f1e93d63157ee16cee342a26a1f3fda0565
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-05-30 14:53:47 -0500

    again change code in shuttle

--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -37,7 +37,7 @@ let nonSuspiciousFunction = function (a) {
     return a;
   }
 };
-console.log("Fuel level: " + checkFuel(nonSuspiciousFunction(220000)));
+
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
 
 //c). Once you figure out how much fuel to pump out, return that value.
@@ -45,7 +45,7 @@ console.log("Fuel level: " + checkFuel(nonSuspiciousFunction(220000)));
 //d). Decide where to best place your function call to gather our new fuel.
 
 // Next, liberate some of that glorious cargo.
-let deckMops = function(array1){
+let robbItems = function(array1){
   let newArr = [];
   for(let i = 0; i < array1.length; i++){
     if(array1[i] === 'satellite' ){
@@ -56,11 +56,10 @@ let deckMops = function(array1){
        array1[i] = 'helium';
     }
   }
+ 
   return newArr;
 };
-console.log(deckMops(cargoHold));
-console.log(cargoHold);
-console.log("Hold status: " + holdStatus(cargoHold));
+
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
 //b). You need to swipe two items from the cargo hold. Choose well. Stealing water ain’t gonna get us rich. Put the swag into a new array and return it from the function.
@@ -71,12 +70,12 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 // Finally, you need to print a receipt for the accountant. Don’t laugh! That genius knows MATH and saves us more gold than you can imagine.
  let irs = function(levelOfFuel, itemsInCargo ){
-   let arr = deckMops(itemsInCargo);
+   let arr = robbItems(itemsInCargo);
    return `Raided ${nonSuspiciousFunction(fuelLevel)} kg of fuel from the tanks, and stole ${arr[0]} and ${arr[1]} from the cargo hold.`
 }
-//console.log(irs(nonSuspiciousFunction(25000), cargoHold));
-
-console.log(irs(220000, ['satellite','gold']));
+console.log(irs(nonSuspiciousFunction(fuelLevel), robbItems(cargoHold)));
+console.log("Fuel level: " + checkFuel(fuelLevel));
+console.log("Hold status: " + holdStatus(cargoHold));
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
 	
 //b). Call your anonymous fuel and cargo functions from within irs.
