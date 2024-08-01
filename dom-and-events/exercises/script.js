d567f55f33418a83e197a8a84b855e3fd2132cae
Dhana bayya <krishnabayya@Dhanas-mac-Air.lan>
2024-08-01 12:15:33 -0500

    exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,22 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
-
-    // Put your code for the exercises here.
-    
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
+  button.addEventListener("click", (event) => {
+    paragraph.innerHTML = "Houston! We have liftoff!";
+  });
+  missionAbort.addEventListener("mouseover", function (event) {
+    event.target.style.backgroundColor = "red";
+  });
+  missionAbort.addEventListener("click", (event) => {
+    let pop = confirm("Are you sure you want to abort the mission?");
+    if (pop == true) {
+      paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+    }
+  });
+  missionAbort.addEventListener("mouseout", function (event) {
+    event.target.style.backgroundColor = "";
+  });
 }
 
 window.addEventListener("load", init);
